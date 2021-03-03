14bb615e8914b55f735d499d04fd6d1e4431c1a7
Taiane <taiane.taveira@soulasalle.com.br>
2021-03-03 19:34:46 -0300

    seçao dois inserida e configurada

--- htdocs/scripts/imagens.js
+++ htdocs/scripts/imagens.js
@@ -1,17 +1,14 @@
-const destaques = document.querySelector('.principal__destaques')
+const principalDestaques = document.querySelector('.principal__destaques')
+const principalBrasil = document.querySelector('.principal__brasil')
+
 
 async function loadingImages() {
     const resposta = await fetch('scripts/data.json');
     let dados = await resposta.json()
 
-    let main = dados.section[0]
+    let destaques = dados.section[0]
 
-    for (let post of main.data) {
-        // let classe = "desta-principal";
-        // if(post == "brasil"){
-        //     classe = 'destaque-brasil'
-        // }
-        
+    for (let post of destaques.data) {
         const divPost = document.createElement('div');
         divPost.innerHTML = `
         <img src = "assets/media/${post.image}">
@@ -20,10 +17,28 @@ async function loadingImages() {
         <h1 class="legenda__titulo">${post.title}</h1>   
         <p> ${post.description}</p><br> 
         </div>
-        ` 
+        `
         divPost.classList.add('destaques')
-       destaques.appendChild(divPost);
+        principalDestaques.appendChild(divPost);
     }
+
+    let brasil = dados.section[1]
+
+    for (let post of brasil.data) {
+        const divPost = document.createElement('div');
+        divPost.innerHTML = `
+        <img src = "assets/media/${post.image}">
+        <div class="legenda">
+        <label> ${post.label}</label><br>  
+        <h1 class="legenda__titulo">${post.title}</h1>   
+        <p> ${post.description}</p><br> 
+        </div>
+        `
+        divPost.classList.add('brasil')
+        principalBrasil.appendChild(divPost);
+    }
+
+
 
 };
 loadingImages()
