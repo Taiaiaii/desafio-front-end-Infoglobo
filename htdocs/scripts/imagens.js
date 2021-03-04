29099123affb386aad7276c19b6b9653fc22b9bb
Taiane <taiane.taveira@soulasalle.com.br>
2021-03-04 17:53:14 -0300

    code review js

--- htdocs/scripts/imagens.js
+++ htdocs/scripts/imagens.js
@@ -7,55 +7,28 @@ async function loadingImages() {
     const resposta = await fetch('scripts/data.json');
     let dados = await resposta.json()
 
-    let destaques = dados.section[0]
-
-    for (let post of destaques.data) {
-        const divPost = document.createElement('div');
-        divPost.innerHTML = `
-        <img src = "assets/media/${post.image}">
-        <div class="legenda">
-        <label> ${post.label}</label><br>  
-        <h1 class="legenda__titulo">${post.title}</h1>   
-        <p> ${post.description}</p><br> 
-        </div>
-        `
-        divPost.classList.add('destaques')
-        principalDestaques.appendChild(divPost);
-    }
-
-    let brasil = dados.section[1]
-
-    for (let post of brasil.data) {
-        const divPost = document.createElement('div');
-        divPost.innerHTML = `
-        <img src = "assets/media/${post.image}">
-        <div class="legenda">
-        <label> ${post.label}</label><br>  
-        <h1 class="legenda__titulo">${post.title}</h1>   
-        <p> ${post.description}</p><br> 
-        </div>
-        `
-        divPost.classList.add('brasil')
-        principalBrasil.appendChild(divPost);
-    }
-
-        let mundo = dados.section[2]
-
-        for (let post of mundo.data) {
-            const divPost = document.createElement('div');
-            divPost.innerHTML = `
-        <img src = "assets/media/${post.image}">
-        <div class="legenda">
-        <label> ${post.label}</label><br>  
-        <h1 class="legenda__titulo">${post.title}</h1>   
-        <p> ${post.description}</p><br> 
-        </div>
-        `
-            divPost.classList.add('mundo')
-            principalMundo.appendChild(divPost);
-        }
-
-
+    carregaFotos(dados, 0, principalDestaques, 'destaques');
+    carregaFotos(dados, 1, principalBrasil, 'brasil');
+    carregaFotos(dados, 2, principalMundo, 'mundo');
 
 };
 loadingImages()
+
+function carregaFotos(dados, posicao, htmlplace, classe) {
+
+    for (let post of dados.section[posicao].data) {
+        const divPost = document.createElement('div');
+        divPost.innerHTML = `
+        <img src = "assets/media/${post.image}">
+        <div class="legenda">
+        <label> ${post.label}</label><br>  
+        <h1 class="legenda__titulo">${post.title}</h1>   
+        <p> ${post.description}</p><br> 
+        </div>
+        `
+        divPost.classList.add(classe)
+        
+        htmlplace.appendChild(divPost);
+    }
+}
+
